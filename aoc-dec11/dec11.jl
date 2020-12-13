af600595134f8b16917a46eb4fdb7ca616ec1353
Lars Hellemo <hellemo@gmail.com>
2020-12-13 17:35:16 +0100

    :zap: Improve performance and clean up a bit

--- aoc-dec11/dec11.jl
+++ aoc-dec11/dec11.jl
@@ -1,13 +1,15 @@
 using Pkg
 Pkg.activate(@__DIR__)
 using PaddedViews
+using Test
 
 td1 = readlines("testinput1.txt")
+quizinput = readlines("input.txt")
 
 function translate_char_val()
     Dict('L' => 0,
-    '#' => 1,
-    '.' => -1)
+         '#' => 1,
+         '.' => -1)
 end
 
 function translate_val_char()
@@ -15,16 +17,9 @@ function translate_val_char()
     Dict(zip(values(translate),keys(translate)))
 end
 
-function preparedata(data)
-    translate = translate_char_val()
-    m = hcat([[translate[c] for c in l] for l in data]...)   
-    (X,Y) = size(m)
-    return m, PaddedView(-2, m, (0:X+1,0:Y+1))
-end
-
 function padded(data)
     translate = translate_char_val()
-    m = hcat([[translate[c] for c in l] for l in data]...)   
+    m = Int8.(hcat([[translate[c] for c in l] for l in data]...)   )
     padded(m)
 end
 
@@ -32,11 +27,6 @@ function padded(m::Matrix)
     (X,Y) = size(m)
     PaddedView(-2, m, (-(X+Y+2):2*(X+Y+1),-(X+Y+2):2*(X+Y+1)))
 end
-
-
-
-m1,p1 = preparedata(td1)
-p1 = padded(td1) 
 
 function occupiedneighbours(thismap, x, y)
     xs = range(-1, step=1, stop=1)
@@ -57,42 +47,7 @@ function next_seat(pv, x, y)
     end 
 end
 
-function next_map(m)
-    (X,Y) = size(m)
-    nm = copy(m)
-    pv = PaddedView(-2, m, (0:X+1,0:Y+1))
-    for x in 1:X, y in 1:Y
-        nm[x, y] = next_seat(pv, x, y)
-    end
-    return nm
-end
-
-
-function game_of_seats(m)
-    nm = next_map(m)
-    if nm == m
-        return sum(nm[nm .> 0])
-    else
-        game_of_seats(nm)
-    end
-end
-
-function dec11_1(input)
-    qi,pv = preparedata(input)
-    # println(qi)
-    game_of_seats(qi)
-end
-
-using Test
-
-quizinput = readlines("input.txt")
-@test dec11_1(td1) == 37
-@test dec11_1(quizinput) == 2204
-
-
-
 # Part II
-
 function next_seat2(pv, x, y)
     occ_neigh = occupiedseen(pv, x, y)
     if pv[x, y] == 0 && occ_neigh == 0
@@ -110,31 +65,25 @@ function occupiedseen(pv,x,y)
 end
 
 function sees_occupied(v) 
-    if length(v) > 0
-        found = findfirst(x->(x>=0), v)
-        if found === nothing
-            return 0
-        else
-            res = v[found]
-        end 
+    it = Iterators.dropwhile(<(0),v)
+    if isempty(it)
+        return 0
     else
-        res = 0
+        return first(it)
     end
-    return res
 end
 
 function diagonal(pv,x,y,east,south)
     X,Y = size(parent(pv))
     MAX = max(X,Y)
-    diag = [pv[x + i* east, y + i*south] for i = 1:MAX]
-    # diag = (pv[x + i* east, y + i*south] for i = 1:MAX)
+    diag = (pv[x + i* east, y + i*south] for i = 1:MAX)
 end
 
-function next_map2(m)
+function next_map2(m,determine_seat)
     (X,Y) = size(parent(m))
     nm = copy(parent(m))
     for x in 1:X, y in 1:Y
-        nm[x, y] = next_seat2(m, x, y)
+        nm[x, y] = determine_seat(m, x, y)
     end
     return padded(nm)
 end
@@ -147,32 +96,38 @@ end
 
 function show_map(m)
     println(string_map(parent(m)))
-    println( "\n---")
+    println( "\n  ---")
 end
 
-function game_of_seats2(m)
-    nm = next_map2(m)
+function game_of_seats2(m,determine_seat=next_seat2)
+    nm = next_map2(m,determine_seat)
     if nm == m
         return nm
     else
-        game_of_seats2(nm)
+        game_of_seats2(nm,determine_seat)
     end
 end
 
-function dec11_2(input)
-    r = game_of_seats2(padded(input))
+function dec11_1(input, determine_seat=next_seat)
+    r = game_of_seats2(padded(input),determine_seat)
     return sum(r[r .> 0])
+end
+
+function dec11_2(input)
+    dec11_1(input,next_seat2)
 end
 
 # Tests
 t1 = readlines("testinput1.txt")
-m2 = next_map2(padded(t1))
+m2 = next_map2(padded(t1),next_seat2)
 t2 = read("testinput2.txt",String)
 @test t2 == string_map(parent(m2))
 
 t3 = read("testinput3.txt",String)
-m3 = next_map2(m2)
+m3 = next_map2(m2,next_seat2)
 @test t3 == string_map(parent(m3))
-
+@test dec11_1(td1) == 37
 @test dec11_2(td1) == 26
+
+@test dec11_1(quizinput) == 2204
 @test dec11_2(quizinput) == 1986
